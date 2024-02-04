a24a5fdf73b28f459591097c6926bdf70a0552a0
josdcarr <84652034+josdcarr@users.noreply.github.com>
2024-02-04 07:31:17 -0500

    Update Temperatura.h

--- Temperatura.h
+++ Temperatura.h
@@ -1,5 +1,3 @@
-
-
 float Temperatura() {
 sensors.requestTemperatures();   //Se envía el comando para leer la temperatura
 float temp= round(sensors.getTempCByIndex(0)); //Se obtiene la temperatura en ºC
